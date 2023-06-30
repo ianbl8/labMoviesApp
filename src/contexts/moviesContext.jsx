1e6b77268cf971165b081c15b5b7ed93698ec6a1
Ian Blake <ian@ianbl8.com>
2023-06-30 16:11:28 +0100

    Tagging upcoming movies as must-watch

--- src/contexts/moviesContext.jsx
+++ src/contexts/moviesContext.jsx
@@ -3,9 +3,11 @@ import React, { useState } from "react";
 export const MoviesContext = React.createContext(null);
 
 const MoviesContextProvider = (props) => {
-  const [myReviews, setMyReviews] = useState( {} );
-
   const [favourites, setFavourites] = useState([]);
+
+  const [mustWatch, setMustWatch] = useState([]);
+
+  const [myReviews, setMyReviews] = useState( {} );
 
   const addToFavourites = (movie) => {
     let updatedFavourites = [...favourites];
@@ -15,9 +17,21 @@ const MoviesContextProvider = (props) => {
     setFavourites(updatedFavourites);
   };
 
-  // We will use this function in a later section
   const removeFromFavourites = (movie) => {
     setFavourites(favourites.filter((mId) => mId !== movie.id));
+  };
+
+  const addToMustWatch = (movie) => {
+    let updatedMustWatch = [...mustWatch];
+    if (!mustWatch.includes(movie.id)) {
+      updatedMustWatch.push(movie.id);
+    }
+    setMustWatch(updatedMustWatch);
+    console.log(updatedMustWatch);
+  };
+
+  const removeFromMustWatch = (movie) => {
+    setMustWatch(mustWatch.filter((mId) => mId !== movie.id));
   };
 
   const addReview = (movie, review) => {
@@ -28,8 +42,11 @@ const MoviesContextProvider = (props) => {
     <MoviesContext.Provider
       value={{
         favourites,
+        mustWatch,
         addToFavourites,
         removeFromFavourites,
+        addToMustWatch,
+        removeFromMustWatch,
         addReview,
       }}
     >
--- src/pages/upcomingMoviesPage.jsx
+++ src/pages/upcomingMoviesPage.jsx
@@ -3,8 +3,7 @@ import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner";
-import IconButton from "@mui/material/IconButton";
-import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import AddToMustWatchIcon from "../components/cardIcons/addToMustWatch";
 
 const UpcomingMoviesPage = (props) => {
   const { data, error, isLoading, isError } = useQuery("upcoming", getUpcomingMovies);
@@ -22,11 +21,7 @@ const UpcomingMoviesPage = (props) => {
 			title="Upcoming Movies"
 			movies={movies}
       action={(movie) => {
-        return (
-          <IconButton aria-label="add to playlist">
-            <PlaylistAddIcon color="primary" fontSize="large" movie={movie} />
-          </IconButton>
-        );
+        return <AddToMustWatchIcon movie={movie} />
       }}
 		/>
 	);
